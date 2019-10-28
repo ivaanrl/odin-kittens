ce0bdfc4765962bdd572f9c33b478da206cd2b71
ivaanrl <ivanrl2010@gmail.com>
2019-10-28 11:11:52 -0300

    Finished app. It's really simple lol

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,55 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json {render :json => @kittens}
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = 'You did it'
+      redirect_to @kitten
+    else 
+      flash.now[:error] = "me cago en vos alberto"
+      render 'new'
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render :json => @kitten}
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "Attributes eddited correctly!"
+      redirect_to @kitten
+    end
+  end
+
+  def destroy
+    Kitten.find(params[:id]).destroy
+    flash[:success] = "Kitten deleted"
+    redirect_to root_url
+  end
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'kittens#index'
+  resources :kittens
+
+  get '/edit', to: 'kittens#edit'
+  get '/new', to: 'kittens#new'
+  get '/destroy', to: 'kitten#destroy'
 end
